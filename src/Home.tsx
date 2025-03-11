593fa78582395d789b69e628787196c8e00f22b0
Jay Hori <jay.hori@gmail.com>
2025-03-12 01:01:45 +0900

    Update sentences

--- src/Home.tsx
+++ src/Home.tsx
@@ -5,40 +5,33 @@ function Home() {
   return (
     <>
       <h1>Web Performance with React</h1>
-      <h2>About</h2>
 
-      <p>
-        This course helps you learn how to optimize React applications for web
-        performance through practical challenges.
-      </p>
-
+      <h2>Challenges</h2>
+      <ul>
+        <li>
+          <Link to="/challenge-1">
+            1: Utilizing the right image format
+          </Link>
+        </li>
+        <li>
+          <Link to="/challenge-2">2: Preloading crutial images</Link>
+        </li>
+        <li>
+          <Link to="/challenge-3">3</Link>
+        </li>
+        <li>
+          <Link to="/challenge-4">4</Link>
+        </li>
+        <li>
+          <Link to="/challenge-5">5</Link>
+        </li>
+      </ul>
       <p>
         <strong>Note:</strong> Each challenge page is intentionally slow and/or
         inefficient. Your task is to identify and fix performance issues.
       </p>
 
-      <h2>Challenges:</h2>
-      <ul>
-        <li>
-          <Link to="/challenge-1">
-            Challenge 1: Utilizing the right image format
-          </Link>
-        </li>
-        <li>
-          <Link to="/challenge-2">Challenge 2: Preloading crutial images</Link>
-        </li>
-        <li>
-          <Link to="/challenge-3">Challenge 3</Link>
-        </li>
-        <li>
-          <Link to="/challenge-4">Challenge 4</Link>
-        </li>
-        <li>
-          <Link to="/challenge-5">Challenge 5</Link>
-        </li>
-      </ul>
-
-      <h2>Final Project:</h2>
+      <h2>Final Project</h2>
       <p>
         <Link to="/final-project">Build a fully optimized web page</Link>
       </p>
@@ -48,6 +41,12 @@ function Home() {
         Solutions are available in the solution git branches. For example, the
         solution for Challenge 1 is available in the{' '}
         <em>solution/challenge-1</em> branch.
+      </p>
+
+      <h2>About</h2>
+      <p>
+        This course helps you learn how to optimize React applications for web
+        performance through practical challenges.
       </p>
     </>
   )
