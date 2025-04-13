ffca7e06512752b876f12f71aed141efbb96f909
Jay Hori <jay.hori@gmail.com>
2025-04-13 23:09:08 +0900

    Add Challenge 5 solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { lazy } from 'react'
 import { Routes, Route, BrowserRouter as Router } from 'react-router-dom'
 import Home from './Home'
 import ChallengeOne from './challenges/ChallengeOne'
 import ChallengeTwo from './challenges/ChallengeTwo'
 import ChallengeThree from './challenges/ChallengeThree'
 import ChallengeFour from './challenges/ChallengeFour'
-import ChallengeFive from './challenges/ChallengeFive'
+const ChallengeFive = lazy(() => import('./challenges/ChallengeFive'))
 import ExampleFetchPriority from './examples/ExampleFetchPriority'
 import ExampleLazyLoadingVersusLCP from './examples/ExampleLazyLoadingVersusLCP'
 import Scoreboard from './examples/Scoreboard'
